2c0eb63921bd5452920c2bceefe27872f2f5a01c
okunlade Jolade <jokunlade@gmail.com>
2023-01-10 18:24:25 +0100

    adding more files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Contact } from "./pages/contact";
+import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from "./context/shop-context";
 
 function App() {
   return (
-    <>
-     <Navbar/>
-    </>
+    <div className="App">
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
+    </div>
   );
 }
 
