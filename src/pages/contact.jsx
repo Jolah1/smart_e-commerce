3f29bf28125b0867da8aca603dee9948410078ac
okunlade Jolade <jokunlade@gmail.com>
2023-01-11 16:13:06 +0100

    changes made

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const Contact = () => {
-  return <div>contact</div>;
+  return <div>
+    <a href="https://www.linkedin.com/in/jolade-okunlade-1840a6138/">Linkedin</a>
+    
+
+  </div>;
 };
